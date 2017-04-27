fff5dbf982dd981d8f21819ebdceb512bb396091
Chris Lucas <chris@chrisjlucas.com>
2017-04-27 16:15:16 -0700

    Remove prod.secret.exs

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,8 @@ config :tvdb_calendar, TVDBCalendar.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
   server: true,
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET") || raise "$SECRET not set"
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -56,7 +57,3 @@ config :logger, level: :info
 #
 #     config :tvdb_calendar, TVDBCalendar.Endpoint, server: true
 #
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
