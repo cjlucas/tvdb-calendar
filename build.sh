b7ad8c15cf989218a9b50b4debe87fb3273e12c0
Chris Lucas <chris@chrisjlucas.com>
2017-04-27 19:29:49 -0700

    Log artifacts

--- build.sh
+++ build.sh
@@ -7,4 +7,6 @@ mix phx.digest
 mix release
 
 #mkdir /artifacts
+echo hithere
+ls /artifacts
 cp -v _build/prod/rel/tvdb_calendar/releases/$APP_VERSION/tvdb_calendar.tar.gz /artifacts/tvdb_calendar-$APP_VERSION.tar.gz
