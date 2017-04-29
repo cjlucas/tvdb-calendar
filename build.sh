07892e550540f68943667f4f1c17496bcf3865a6
Chris Lucas <chris@chrisjlucas.com>
2017-04-29 09:35:34 -0700

    Build: Add support for UP_FROM envvar

--- build.sh
+++ build.sh
@@ -4,6 +4,13 @@ mix deps.get
 npm i
 brunch build --production
 mix phx.digest
-mix release --upgrade
+
+if [[ $UP_FROM != "" ]]; then
+    echo "Building release to upgrade from version $UP_FROM"
+    mix release --upgrade --upfrom=$UP_FROM
+else
+    echo "$UP_FROM was not specified, won't specifying --upgrade"
+    mix release
+fi
 
 cp -v _build/prod/rel/tvdb_calendar/releases/$APP_VERSION/tvdb_calendar.tar.gz /artifacts/tvdb_calendar-$APP_VERSION.tar.gz
